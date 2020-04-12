84f652ea808664ee38c18295e409cebfd65a1183
Pila louis <plouisasobo@gmail.com>
2020-04-12 21:19:14 +0100

    requirement file added

--- config.py
+++ config.py
@@ -8,6 +8,7 @@ DEBUG = True
 
 # Database connection string
 SQLALCHEMY_DATABASE_URI = 'postgres://pilalouis@localhost:5432/movies'
+#postgres://mizkaqudrvclaq:83ff9158a9332ddcab66f03a3a387c64554f49e916a1746d840e3c067818dad4@ec2-52-6-143-153.compute-1.amazonaws.com:5432/dckkrj3786hpta
 
 # Supress warning
 SQLALCHEMY_TRACK_MODIFICATIONS = False
